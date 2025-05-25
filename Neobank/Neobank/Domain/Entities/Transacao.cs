715d3281b7c4974d879abf56df3a3f5a63f3bf0b
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-05-25 20:32:51 -0300

    feat: salvando o nome do sender e do receiver nas transacoes

--- Neobank/Neobank/Domain/Entities/Transacao.cs
+++ Neobank/Neobank/Domain/Entities/Transacao.cs
@@ -8,7 +8,9 @@ public class Transacao
     public string Id { get; set; } = Guid.NewGuid().ToString(); 
     public string Tipo { get; set; } = string.Empty;
     public string SenderId { get; set; } = string.Empty;
+    public string SenderName { get; set; } = string.Empty;
     public string ReceiverId { get; set; } = string.Empty;
+    public string ReceiverName { get; set; } = string.Empty;
     public DateTime Data { get; set; }
     public int Value { get; set; } = 0;
 }
